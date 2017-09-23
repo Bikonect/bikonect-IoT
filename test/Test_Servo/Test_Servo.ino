0c3bff400a9d05fc25311c2b20ebd93a49ad830d
Eduardo Araujo <eduardo.araujo0@outlook.com>
2017-09-23 11:51:59 -0300

    Configurado 2 servo no teste com servo

--- test/Test_Servo/Test_Servo.ino
+++ test/Test_Servo/Test_Servo.ino
@@ -1,26 +1,34 @@
 #include <Servo.h>
 
-#define SERVO 6
+#define SERVO_1 5
+#define SERVO_2 6
 
-Servo s;
+Servo s1;
+Servo s2;
 int pos;
 
 void setup () {
-  s.attach(SERVO);
+  s1.attach(SERVO_1);
+  s2.attach(SERVO_2);
+  
   Serial.begin(9600);
-  s.write(0);
+  
+  s1.write(0);
+  s2.write(0);
 }
 
 void loop() {
   for (pos = 0; pos < 180; pos++) {
-    s.write(pos);
+    s1.write(pos);
+    s2.write(pos);
     delay(1);
   }
   
   delay(1000);
   
   for (pos = 180; pos >= 0; pos--) {
-    s.write(pos);
+    s1.write(pos);
+    s2.write(pos);
     delay(1);
   }
 
